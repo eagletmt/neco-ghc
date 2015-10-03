0550fea80e9c958a479067805bcf98e294bb2e32
Shougo Matsushita <Shougo.Matsu@gmail.com>
2015-10-03 11:09:16 +0900

    Fix #57 deoplete source

--- rplugin/python3/deoplete/sources/ghc.py
+++ rplugin/python3/deoplete/sources/ghc.py
@@ -5,6 +5,7 @@
 
 from .base import Base
 import deoplete.util
+import re
 
 class Source(Base):
     def __init__(self, vim):
@@ -14,6 +15,7 @@ class Source(Base):
         self.mark = '[ghc]'
         self.filetypes = ['haskell', 'lhaskell']
         self.is_bytepos = True
+        self.min_pattern_length = 0
 
         self.executable_ghc = self.vim.eval('executable("ghc-mod")')
         self.vim.command('call necoghc#boot()')
@@ -27,6 +29,15 @@ class Source(Base):
                                      ))
 
     def gather_candidates(self, context):
+        # force auto-completion on importing functions
+        if (not re.search('import\s+|[^. \t0-9]\.\w*$',
+                          context['input'])) and \
+            context['event'] != 'Manual' \
+                        and len(context['complete_str']) < \
+                            self.vim.eval(
+                                'g:deoplete#auto_completion_start_length') :
+                return []
+
         return self.vim.eval("necoghc#get_complete_words("
                              + str(context['complete_position']) + ",'"
                              + str(context['complete_str']) + "')")
